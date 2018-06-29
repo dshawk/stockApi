ab01a5121587a5491e7a6e4ab70efd6f4ade6d87
git <n.timirbulatov@info-link.ru>
2018-06-29 13:55:46 +0300

    add SwiftyJSON

---
+++ Podfile
@@ -0,0 +1,5 @@
+use_frameworks!
+
+target 'stockApi' do
+  pod 'SwiftyJSON'
+end
---
+++ Pods/Target Support Files/Pods-stockApi/Pods-stockApi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_stockApi : NSObject
+@end
+@implementation PodsDummy_Pods_stockApi
+@end
